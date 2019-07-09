db921b7c81751ea8766986d1e08c181b39557fd6
Istvan Bartha <bartha.pityu@gmail.com>
2019-07-09 21:59:43 +0200

    Add strictBatchWeightedByteString

--- src/main/scala/lame/Batch.scala
+++ src/main/scala/lame/Batch.scala
@@ -2,9 +2,16 @@ package lame
 
 import akka.stream.scaladsl.Flow
 import scala.concurrent.duration._
+import akka.util.ByteString
 
 object Batch {
-  def strictBatchWeighted[T](
+
+  val reduceByteStrings = (s: Seq[ByteString]) => {
+    val builder = ByteString.newBuilder
+    s.foreach(b => builder.append(b))
+    builder.result
+  }
+  def strictBatchWeightedR[T](
       maxWeight: Long,
       cost: T => Long,
       maxDuration: FiniteDuration = 1 seconds
@@ -12,4 +19,22 @@ object Batch {
     Flow[T]
       .groupedWeightedWithin(maxWeight, maxDuration)(cost)
       .map(_.reduce(reduce))
+
+  def strictBatchWeighted[T, K](
+      maxWeight: Long,
+      cost: T => Long,
+      maxDuration: FiniteDuration = 1 seconds
+  )(reduce: Seq[T] => K): Flow[T, K, akka.NotUsed] =
+    Flow[T]
+      .groupedWeightedWithin(maxWeight, maxDuration)(cost)
+      .map(reduce)
+
+  def strictBatchWeightedByteStrings(
+      maxWeight: Long,
+      cost: ByteString => Long,
+      maxDuration: FiniteDuration = 1 seconds
+  ): Flow[ByteString, ByteString, akka.NotUsed] =
+    strictBatchWeighted[ByteString, ByteString](maxWeight, cost, maxDuration)(
+      reduceByteStrings
+    )
 }
